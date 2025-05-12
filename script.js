13c41c73364cca08b2e9054ac7e1141f02f7a083
anilkumar784 <kumaranil23234@gmail.com>
2025-05-12 21:32:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set the URL to redirect to after a delay (in milliseconds)
 const redirectDelay = 10; 
-const redirectUrl = "tel:+1-855-914-4200"; // Change this to the desired URL
+const redirectUrl = "tel:+1-888-768-8944"; // Change this to the desired URL
 
 // Function to redirect to another URL
 function redirectToURL() {
